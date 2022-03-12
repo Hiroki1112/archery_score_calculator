04b18d62f5bde05556e8f391e57844cb450c0960
Hiroki1112 <pystdy@yahoo.co.jp>
2022-03-12 23:10:11 +0900

    add : implement toPoint()

--- lib/archery_score_calculator.dart
+++ lib/archery_score_calculator.dart
@@ -1,7 +1,17 @@
 library archery_score_calculator;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// A Calculator for archery.
+/// Score is passed as string, and converted to int.
+class ArcheryScoreCalculator {
+  List<String> _scoreList = [];
+
+  int toPoint(String point) {
+    if (point == 'X') {
+      return 10;
+    } else if (point == 'M') {
+      return 0;
+    } else {
+      return int.parse(point);
+    }
+  }
 }
--- test/archery_score_calculator_test.dart
+++ test/archery_score_calculator_test.dart
@@ -3,10 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:archery_score_calculator/archery_score_calculator.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('test of toPoint()', () {
+    final calculator = ArcheryScoreCalculator();
+    expect(calculator.toPoint('0'), 0);
+    expect(calculator.toPoint('1'), 1);
+    expect(calculator.toPoint('2'), 2);
+    expect(calculator.toPoint('3'), 3);
+    expect(calculator.toPoint('4'), 4);
+    expect(calculator.toPoint('5'), 5);
+    expect(calculator.toPoint('6'), 6);
+    expect(calculator.toPoint('7'), 7);
+    expect(calculator.toPoint('8'), 8);
+    expect(calculator.toPoint('9'), 9);
+    expect(calculator.toPoint('10'), 10);
+    expect(calculator.toPoint('X'), 10);
+    expect(calculator.toPoint('M'), 0);
   });
 }
